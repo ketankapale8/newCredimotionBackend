5657d2d9340523688f6ab93a071f4cd1cfabf9cd
ketankapale8 <ketan.kapale@gmail.com>
2023-09-10 09:36:43 +0530

    changes

--- middleware/jwt.js
+++ middleware/jwt.js
@@ -4,12 +4,18 @@ import createError from "../utils/createError.js";
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.accessToken;
   if (!token) return next(createError(401,"You are not authenticated!"))
+  const options = {
+    httpOnly:true,
+    
+    
+    expires : new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES *24 *60 *600 * 1000)
+}
 
 
-  jwt.verify(token, process.env.JWT_SECRET, async (err, payload) => {
+  jwt.verify(token, process.env.JWT_SECRET, options, async (err, payload) => {
     if (err) return next(createError(403,"Token is not valid!"))
     req.userId = payload.id;
-    req.isSeller = payload.isSeller;
+  
     next()
   });
 };
